9b1b10b7656e947ec233d3da68ff196645a001c3
João Pedro <jonas0378@gmail.com>
2022-04-20 19:09:45 -0300

    feat: button basic component

---
+++ src/components/Button/Button.style.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+import {ButtonProps} from './Button';
+import {colors} from '../../styles/colors';
+
+export const backgroundColors: Record<ButtonProps['type'], string> = {
+  primary: colors.primary,
+  secondary: colors.white,
+  half: colors.primary,
+  danger: colors.danger,
+};
+export const titleColors: Record<ButtonProps['type'], string> = {
+  primary: colors.white,
+  secondary: colors.black,
+  half: colors.white,
+  danger: colors.danger,
+};
+export const spinnerColors: Record<ButtonProps['type'], string> = {
+  primary: colors.white,
+  secondary: colors.primary,
+  half: colors.white,
+  danger: colors.danger,
+};
+
+export const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
+  background-color: ${props =>
+    props.type !== 'danger' ? backgroundColors[props.type] : 'transparent'};
+  border: ${props => (props.type === 'danger' ? `2px ${colors.danger}` : null)};
+  opacity: ${p => (p.loading ? 0.5 : 1)};
+  height: 45px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  align-self: ${props => (props.type === 'half' ? 'center' : 'flex-start')};
+  width: ${props => (props.type === 'half' ? 50 : 100)}%;
+`;
+
+export const Label = styled.Text<ButtonProps>`
+  color: ${props => titleColors[props.type]};
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const Spinner = styled.ActivityIndicator``;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {TouchableOpacityProps} from 'react-native';
+import {ButtonContainer, Label, Spinner, spinnerColors} from './Button.style';
+
+export interface ButtonProps extends TouchableOpacityProps {
+  type: 'primary' | 'secondary' | 'half' | 'danger';
+  title?: string;
+  loading?: boolean;
+  onPress?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = props => {
+  const {type = 'primary', title, onPress, loading, disabled} = props;
+
+  return (
+    <ButtonContainer
+      onPress={onPress}
+      type={type}
+      disabled={loading || disabled}
+      activeOpacity={0.8}>
+      {loading ? (
+        <Spinner color={spinnerColors[type]} />
+      ) : (
+        <Label type={type}>{title}</Label>
+      )}
+    </ButtonContainer>
+  );
+};
